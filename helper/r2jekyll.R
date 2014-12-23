4743ce8c6bd5f606ba6f6ff91b16be84a3f17c9f
nicolewhite <nicole.white@neotechnology.com>
2014-12-23 09:44:51 -0800

    automatically add to _posts

--- helper/r2jekyll.R
+++ helper/r2jekyll.R
@@ -1,22 +1,14 @@
 #!/usr/bin/env Rscript
-args = commandArgs(TRUE)
-
 setwd('../_drafts')
 library(knitr)
 
-if(args[1] == "publish") {
-  dir = paste0("../_posts/", Sys.Date(), "-")
-} else if(args[1] == "preview") {
-  dir = "../_drafts/"
-} else {
-  stop("You must publish or preview a .Rmd file.")
-}
-
-file = args[2]
+args = commandArgs(TRUE)
+file = args[1]
 
 if(!grepl(".Rmd", file)) {
   stop("You must specify a .Rmd file.")
 }
 
+dir = paste0("../_posts/", Sys.Date(), "-")
 output = paste0(dir, sub('.Rmd', '.md', file))
 knit(file, output)
